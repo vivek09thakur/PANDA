8d7e55169fc764afa54e88de77b81529288fb4cb
Vivek Thakur <20509.vivek@gpranchi.org>
2024-05-10 19:53:15 +0530

    commit

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ from main.transform import PANDA
 model = PANDA(['Train/Prompts/train_prompts.txt', 50])
 model.pretrained('saved_model/panda-25k-2.5-lstm-lm')
 
-for i in range(5): # inference for 5 turns
-    user_prompt = input('==> ')
-    print(model.infer(user_input=user_prompt))
+for i in range(5): 
+    user_prompt = input('=> ')
+    completion = model.infer(user_input=user_prompt)
+    print("=> ",completion)
