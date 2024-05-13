10e66c5513d3cf8626a800bc34c5db3db5d20fe1
Vivek Thakur <20509.vivek@gpranchi.org>
2024-05-14 04:38:40 +0530

    commit

--- main.py
+++ main.py
@@ -5,19 +5,20 @@ import argparse
 argparser = argparse.ArgumentParser()
 argparser.add_argument('--train', action='store_true')
 argparser.add_argument('--infer', action='store_true')
-
+argparser.add_argument('--train_prompts', type=str)
+argparser.add_argument('--epochs', type=int)
+argparser.add_argument('--prompt_file', type=str)
+argparser.add_argument('--pretrained_model', type=str)
+argparser.add_argument('--infer_rounds', type=int)
 args = argparser.parse_args()
+
 if args.train:
-    argparser.add_argument('--train_prompts', type=str)
-    argparser.add_argument('--epochs', type=int)
     trainer = TRAINER(args.train_prompts)
     trainer.build_model(args.epochs)
     trainer.build_model(epochs=args.epochs)
 
 elif args.infer:
-    argparser.add_argument('--prompt_file', type=str)
-    argparser.add_argument('--pretrained_model', type=str)
-    argparser.add_argument('--infer_rounds', type=int)
+    
     model = PANDA([args.prompt_file, 50])
     model.pretrained(args.pretrained_model)
     
